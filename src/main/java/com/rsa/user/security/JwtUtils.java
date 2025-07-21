638dad9dad5f056f2c93f65286c76cb18ddbee7b
Nivas Ram <nivasrampothiraju@gamil.com>
2025-07-21 21:19:49 +0530

    rsa-2 token setup

--- src/main/java/com/rsa/user/security/JwtUtils.java
+++ src/main/java/com/rsa/user/security/JwtUtils.java
@@ -2,33 +2,35 @@ package com.rsa.user.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import jakarta.servlet.*;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtils {
-    private static final String SECRET = "secrets";
-    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    private final Key key;
     private static final long EXPIRATION_TIME = 1000 * 60 * 60;
+
+    public JwtUtils(@Value("${jwt.secret-key}") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(String email){
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(io.jsonwebtoken.SignatureAlgorithm.HS256, SECRET)
+                .signWith(key)
                 .compact();
     }
 
-    public String extractEmail(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET)
+    public void extractEmail(String token) {
+        Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
